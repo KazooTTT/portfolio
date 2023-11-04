446a4b0303189ec387bb0909904bbe6e272e5542
KazooTTT <yjwang.echo@gmail.com>
2023-11-04 23:20:03 +0800

    feat: add social media card list

---
+++ app/components/SocialMediaList.tsx
@@ -0,0 +1,61 @@
+import { Icons } from "@/components/icons";
+import config from "@/lib/config";
+import Link from "next/link";
+
+const SocialMediaItem = ({
+  platform,
+  value,
+}: {
+  platform: string;
+  value: string;
+}) => {
+  switch (platform) {
+    case "github":
+      return (
+        <Link href={`https://github/${value}`} key={platform}>
+          <Icons.gitHub className="h-6 w-6" />
+        </Link>
+      );
+    case "bilibili":
+      return (
+        <Link href={value} key={platform}>
+          <Icons.bilibili className="h-6 w-6" />
+        </Link>
+      );
+    case "weibo":
+      return (
+        <Link href={value} key={platform}>
+          <Icons.weibo className="h-6 w-6" />
+        </Link>
+      );
+    default:
+      return (
+        <Link href={value} key={platform}>
+          <Icons.external className="h-6 w-6" />
+        </Link>
+      );
+  }
+};
+
+const SocialMediaList = () => {
+  return (
+    <div className="flex space-x-3 items-center">
+      {Object.entries(config.socialMedia).map((item) => {
+        const [platform, value] = item;
+        console.log("%c Line:32 🍓 item", "color:#3f7cff", item);
+        return (
+          <div
+            className="border-0 relative inline-flex items-center justify-center p-0.5 overflow-hidden font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+            key={platform}
+          >
+            <span className="relative px-2 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              <SocialMediaItem platform={platform} value={value} />
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SocialMediaList;
--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,9 @@
 const config = {
-  github: {
-    username: "kazoottt",
+  socialMedia: {
+    github: "kazoottt",
+    bilibili: "https://space.bilibili.com/391236013",    weibo: "https://weibo.com/u/7796753876",
+    twitter: "kazoottt",
+    email: "kazoottt",
   },
 };
 
