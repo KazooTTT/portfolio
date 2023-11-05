b3a93371249f58d826c4403c83cf6f3f8d0d810d
KazooTTT <yjwang.echo@gmail.com>
2023-11-05 13:01:48 +0800

    feat: add email icon

--- app/components/SocialMediaList.tsx
+++ app/components/SocialMediaList.tsx
@@ -13,25 +13,31 @@ const SocialMediaItem = ({
     case "github":
       return (
         <Link href={`https://github/${value}`} key={platform}>
-          <Icons.gitHub className="h-6 w-6" />
+          <Icons.gitHub className="h-4 w-4" />
         </Link>
       );
     case "bilibili":
       return (
         <Link href={value} key={platform}>
-          <Icons.bilibili className="h-6 w-6" />
+          <Icons.bilibili className="h-4 w-4" />
         </Link>
       );
     case "weibo":
       return (
         <Link href={value} key={platform}>
-          <Icons.weibo className="h-6 w-6" />
+          <Icons.weibo className="h-4 w-4" />
+        </Link>
+      );
+    case "email":
+      return (
+        <Link href={`mailto:${value}`} key={platform}>
+          <Icons.email className="h-4 w-4" />
         </Link>
       );
     default:
       return (
         <Link href={value} key={platform}>
-          <Icons.external className="h-6 w-6" />
+          <Icons.external className="h-4 w-4" />
         </Link>
       );
   }
@@ -39,13 +45,13 @@ const SocialMediaItem = ({
 
 const SocialMediaList = () => {
   return (
-    <div className="flex space-x-3 items-center">
+    <div className="flex space-x-3 items-center flex-wrap">
       {Object.entries(config.socialMedia).map((item) => {
         const [platform, value] = item;
         console.log("%c Line:32 🍓 item", "color:#3f7cff", item);
         return (
           <div
-            className="border-0 relative inline-flex items-center justify-center p-0.5 overflow-hidden font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+            className="border-0 relative inline-flex items-center justify-center p-0.5 overflow-hidden font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 hover:cursor-pointer"
             key={platform}
           >
             <span className="relative px-2 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
--- lib/config.ts
+++ lib/config.ts
@@ -1,9 +1,10 @@
 const config = {
   socialMedia: {
     github: "kazoottt",
-    bilibili: "https://space.bilibili.com/391236013",    weibo: "https://weibo.com/u/7796753876",
+    bilibili: "https://space.bilibili.com/391236013",
+    weibo: "https://weibo.com/u/7796753876",
+    email: "yjwang.echo@gmail.com",
     twitter: "kazoottt",
-    email: "kazoottt",
   },
 };
 
